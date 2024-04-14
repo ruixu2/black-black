e203823b034b4a18a9bce5c954648705a641eeb0
sysutae <34615161+ruixu2@users.noreply.github.com>
2024-04-15 00:09:28 +0800

    成功加密一个文件

--- encode_file.py
+++ encode_file.py
@@ -32,7 +32,6 @@ def calculate_file_md5(file_path):
 def encrypt_file(input_filename, output_filename, key):  
     """  
     加密文件。  
-  
     :param input_filename: 要加密的文件名。  
     :param output_filename: 加密后的文件名。  
     :param key: 加密密钥，应为16（AES128）、24（AES192）或32（AES256）字节长。  
@@ -42,20 +41,17 @@ def encrypt_file(input_filename, output_filename, key):
     # 读取原始文件内容  
     with open(input_filename, "rb") as f:  
         plaintext = f.read()  
-      
     # 对明文进行填充  
     padded_text = pad(plaintext, AES.block_size)  
-      
     # 加密填充后的文本  
     ciphertext = cipher.encrypt(padded_text)  
-      
     # 获取IV（初始化向量），通常与密文一起保存  
     iv = cipher.iv  
-      
     # 将IV和密文一起写入输出文件  
-    with open(output_filename, "wb") as f:  
-        f.write(iv)  
-        f.write(ciphertext)  
+    with open(output_filename, "wb") as f2:  
+        f2.write(iv)  
+        f2.write(ciphertext)  
+    
 def write_info(file_md5,en_key,file_name):
     if not os.path.exists("info.json"):
         f=open("info.json",mode='w')
@@ -74,7 +70,8 @@ def write_info(file_md5,en_key,file_name):
     
 
 # 示例用法  
-file_name="联合实名举报211高校华中某业大学动物Y养系黄某若教授学术造假行为.rar"
+# file_name="联合实名举报211高校华中某业大学动物Y养系黄某若教授学术造假行为.rar"
+file_name="北邮-郑凤-举报信.pdf"
 file_path = f'./{file_name}'  # 替换为你的文件路径  
 md5_value=None
 try:  
@@ -91,10 +88,10 @@ key = get_random_bytes(16)  # 对于AES-128，密钥长度应为16字节
 print(key)
 write_info(md5_value,key,file_name)
 # # 加密文件  
-# input_file = 'plaintext.txt'  
-# encrypted_file = 'encrypted.bin'  
-# encrypt_file(input_file, encrypted_file, key)  
-# print(f"文件 {input_file} 已加密为 {encrypted_file}")  
+input_file=file_path
+encrypted_file = f'./{md5_value}/{md5_value}'  
+encrypt_file(input_file, encrypted_file, key)  
+print(f"文件 {input_file} 已加密为 {encrypted_file}")  
   
 # # 解密文件  
 # decrypted_file = 'decrypted.txt'  
